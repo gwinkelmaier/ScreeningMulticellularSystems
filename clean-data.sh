19b62820a758c90a85c670ce7048bd53a197b9a0
Garrett Winkelmaier <gwinkelmaier@nevada.unr.edu>
2019-08-01 10:08:33 -0700

    removal

--- clean-data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find /home/gwinkelmaier/parvin-labs/nuc-seg/data -type f -path '*/inputs/*' -name '*output*' -delete
-find /home/gwinkelmaier/parvin-labs/nuc-seg/data -type f -path '*/inputs/*' -name '*Fusion*' -delete
